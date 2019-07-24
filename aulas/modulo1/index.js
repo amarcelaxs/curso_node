8420c40d643b89fc357479316632b046cf451522
Ana Xavier <ana.xavier@m2mrj.local>
2019-07-24 14:00:37 -0300

    aula modulo1 -6

---
+++ aulas/modulo1/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+  return next();
+};
+
+app.get("/", logMiddleware, (req, res) => {
+  return res.send(`Bem vindo, ${req.query.name}`);
+});
+
+app.get("/nome/:name", (req, res) => {
+  return res.json({
+    message: `Bem-vindo,${req.params.name}`
+  });
+
+  /*
+  app.get("/nome/:name", (req, res) => {
+  return res.send(`Bem - vindo, ${req.params.name}`);
+  
+  });
+  */
+
+
+});
+
+
+app.listen(3000);
